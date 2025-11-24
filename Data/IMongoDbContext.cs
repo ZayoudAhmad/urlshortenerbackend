12daa8bdb13f475e36a134c98923450ee6d35f83
Ahmed Zayoud <ahmed.zayoud@se.linedata.com>
2025-11-24 10:28:12 +0100

    added mongodb context

---
+++ Data/IMongoDbContext.cs
@@ -0,0 +1,9 @@
+using MongoDB.Driver;
+using urlshortenerbackend.Models;
+
+namespace urlshortenerbackend.Data;
+
+public interface IMongoDbContext
+{
+    IMongoCollection<ShortUrl> ShortUrls { get; }
+}
---
+++ Data/MongoDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using urlshortenerbackend.Models;
+
+namespace urlshortenerbackend.Data;
+
+public class MongoDbContext : IMongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(IOptions<MongoDbSettings> settings)
+    {
+        var client = new MongoClient(settings.Value.ConnectionString);
+        _database = client.GetDatabase(settings.Value.DatabaseName);
+    }
+    public IMongoCollection<ShortUrl> ShortUrls => _database.GetCollection<ShortUrl>("ShortUrls");
+}
